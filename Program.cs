3fa2a75211cee6ee9e6e3ed6956d21a05db3071f
Levente Major <leventemajor36@gmail.com>
2023-09-14 10:24:56 +0200

    FInished task 8

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace CA230914_01
             using var sr = new StreamReader(
                 @"..\..\..\src\fob2016.txt",
                 Encoding.UTF8);
+
             while (!sr.EndOfStream)versenyzok.Add(new Versenyzo(sr.ReadLine()));
 
             Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
@@ -22,7 +23,35 @@ namespace CA230914_01
             float nokAranya = nokSzama / (float)versenyzok.Count * 100;
             Console.WriteLine($"4. feladat: Női versenyzpők aránya: {nokAranya:0.00}%");
 
+            Console.WriteLine("6.Feladat: ");
+            var nb = versenyzok
+                .Where(v => !v.Kategoria)
+                .OrderBy(v => v.Osszpont)
+                .Last();
+            Console.WriteLine($"\tNév: {nb.Nev}");
+            Console.WriteLine($"\tEgyesület: {nb.Egyesulet}");
+            Console.WriteLine($"\tÖsszpont: {nb.Osszpont}");
 
+            Console.WriteLine("7.Feladat: kiírás");
+            using var sw = new StreamWriter(
+                path: @"..\..\..\src\osszpontFF.txt",
+                append: false,
+                Encoding.UTF8);
+
+            foreach (var v in versenyzok)
+            {
+                if (v.Kategoria) sw.WriteLine($"{v.Nev};{v.Osszpont}");
+            }
+
+            Console.WriteLine("8.Feladat: ");
+            var egyesuletek = versenyzok
+                .GroupBy(v => v.Egyesulet)
+                .Where(x => x.Count() < 3 && x.Key is not null);
+
+            foreach (var kvp in egyesuletek)
+            {
+                Console.WriteLine($"\t {kvp.Key} - {kvp.Count()}");
+            }
         }
     }
 }
