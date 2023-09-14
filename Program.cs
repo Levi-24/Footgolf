062deb15668189d200ddb2b2952232e76409b81c
Levente Major <leventemajor36@gmail.com>
2023-09-14 09:52:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CA230914_01
+{
+    class Program
+    {
+        static void Main()
+        {
+            var versenyzok = new List<Versenyzo>();
+            using var sr = new StreamReader(
+                @"..\..\..\src\fob2016.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream)versenyzok.Add(new Versenyzo(sr.ReadLine()));
+
+            Console.WriteLine($"3. feladat: Versenyzők száma: {versenyzok.Count}");
+
+            int nokSzama = versenyzok.Count(v => !v.Kategoria);
+            float nokAranya = nokSzama / (float)versenyzok.Count * 100;
+            Console.WriteLine($"4. feladat: Női versenyzpők aránya: {nokAranya:0.00}%");
+
+
+        }
+    }
+}
